b293570b6d75997d5d6c18f43d67705769bcaa50
Ibraim Killiny <ebrahim.4190113@eng.modern-academy.edu.eg>
2026-05-02 22:37:02 +0300

    the web app is ready

--- app.py
+++ app.py
@@ -28,13 +28,19 @@ def predict_api():
     # Perform prediction using the loaded regression model
     output = regmodel.predict(new_data)
     
-    # Round the final result to 4 decimal places
-    final_output = round(float(output[0]), 4)
-    
     # Print the result to the terminal for debugging purposes
-    print(f"Prediction: {final_output}")
+    print(f"Prediction: {output[0]}")
     
-    return jsonify(final_output)
+    return jsonify(output[0])
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
